a0e752d84249025a4367ad4966b223b2f2bdce2d
ScottGrieve <pokecarft@gmail.com>
2021-11-25 11:26:03 +0000

    Setup the files and started work on the functions

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # calculator
+
+This is the final project of the foundation section of The Odin Project. It will involve creating a calculator script, and then linking it to a UI.
---
+++ script.js
@@ -0,0 +1,22 @@
+// setting up the initial functions
+
+let add = function(a,b) {
+    return a + b;
+}
+console.log(add(5, 5));
+let subtract = function(a,b) {
+    return a - b;
+}
+console.log(subtract(5, 5));
+let multiply = function(a, b) {
+    return a * b;
+}
+console.log(multiply(5, 5));
+let divide = function(a, b) {
+    return a / b;
+}
+console.log(divide(5, 5));
+
+let operate = function(a, b, operator) {
+    
+}
