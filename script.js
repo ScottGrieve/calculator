f36383568f2f0b099beef760d4f45e7126b322f2
ScottGrieve <pokecarft@gmail.com>
2021-11-25 17:56:17 +0000

    HTML calculator set up

--- script.js
+++ script.js
@@ -3,20 +3,25 @@
 let add = function(a,b) {
     return a + b;
 }
-console.log(add(5, 5));
+
 let subtract = function(a,b) {
     return a - b;
 }
-console.log(subtract(5, 5));
 let multiply = function(a, b) {
     return a * b;
 }
-console.log(multiply(5, 5));
 let divide = function(a, b) {
     return a / b;
 }
-console.log(divide(5, 5));
-
-let operate = function(a, b, operator) {
-    
+// a function that takes 2 numbers and uses the selected operator
+let operate = function(num1, num2, operator) {
+    if (operator === 'add') {
+        console.log(add(num1, num2))
+    } else if (operator === 'subtract') {
+        console.log(subtract(num1, num2))
+    } else if (operator === 'multiply') {
+        console.log(multiply(num1, num2))
+    } else if (operator === 'divide') {
+        console.log(divide(num1, num2))
+    } 
 }
